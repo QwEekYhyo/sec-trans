d6918197eecda846f1370e131f8cca61be68c461
Logan Lucas <logan.lucas83@gmail.com>
2025-01-03 23:03:41 +0100

    remove stupid shit

--- src/test_client.c
+++ src/test_client.c
@@ -1,32 +1,15 @@
+#include "../include/client.h"
+
 #include <dlfcn.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 static char* msg = "Hello, world!";
 
 int main() {
-    void* handle;
-    int (*sndmsg)(char*, int port);
-    char* error;
-
-    handle = dlopen("./libclient.so", RTLD_LAZY);
-    if (!handle) {
-        fprintf(stderr, "%s\n", dlerror());
-        exit(EXIT_FAILURE);
-    }
-
-    *(void**)(&sndmsg) = dlsym(handle, "sndmsg");
-    if ((error = dlerror()) != NULL) {
-        fprintf(stderr, "%s\n", error);
-        dlclose(handle);
-        exit(EXIT_FAILURE);
-    }
-
     printf("Sending message to server...\n");
     int error_code = sndmsg(msg, 8080);
 
     printf("Sent message with code: %d\n", error_code);
 
-    dlclose(handle);
     return 0;
 }
--- src/test_server.c
+++ src/test_server.c
@@ -1,42 +1,10 @@
+#include "../include/server.h"
+
 #include <dlfcn.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 int main() {
-    void* handle;
-    int (*startserver)(int);
-    int (*stopserver)();
-    int (*getmsg)(char*);
-    char* error;
-
-    handle = dlopen("./libserver.so", RTLD_LAZY);
-    if (!handle) {
-        fprintf(stderr, "%s\n", dlerror());
-        exit(EXIT_FAILURE);
-    }
-
-    *(void**)(&startserver) = dlsym(handle, "startserver");
-    if ((error = dlerror()) != NULL) {
-        fprintf(stderr, "%s\n", error);
-        dlclose(handle);
-        exit(EXIT_FAILURE);
-    }
-
-    *(void**)(&stopserver) = dlsym(handle, "stopserver");
-    if ((error = dlerror()) != NULL) {
-        fprintf(stderr, "%s\n", error);
-        dlclose(handle);
-        exit(EXIT_FAILURE);
-    }
-
-    *(void**)(&getmsg) = dlsym(handle, "getmsg");
-    if ((error = dlerror()) != NULL) {
-        fprintf(stderr, "%s\n", error);
-        dlclose(handle);
-        exit(EXIT_FAILURE);
-    }
-
     printf("Starting server on port 8080...\n");
     if (startserver(8080) == 0) {
         printf("Server started successfully.\n");
@@ -61,6 +29,5 @@ int main() {
         printf("Failed to stop the server.\n");
     }
 
-    dlclose(handle);
     return 0;
 }
